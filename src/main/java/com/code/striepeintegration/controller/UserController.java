f60a0bd0a37932ba2f21b64dce04584234a77faa
masumcse1 <optinfra.masum@gmail.com>
2024-05-23 16:25:34 +0600

    swagger  JPA Rest done

---
+++ src/main/java/com/code/striepeintegration/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.code.striepeintegration.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(
+        name = "CRUD REST APIs for User Resource",
+        description = "CRUD REST APIs - Create User, Update User, Get User, Get All Users, Delete User"
+)
+@RestController
+@RequestMapping("api/users")
+public class UserController {
+
+
+
+    @GetMapping
+    public String getAllUsers(){
+
+        return "masumok";
+    }
+
+}
