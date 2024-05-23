768516d2a9b989d603720626d9be0ca1d6f6496e
Gulam Samdani <optinfra.masum@gmail.com>
2024-05-23 16:30:27 +0600

    Update UserController.java

--- src/main/java/com/code/striepeintegration/controller/UserController.java
+++ src/main/java/com/code/striepeintegration/controller/UserController.java
@@ -14,7 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
 
-
+     @Operation(
+            summary = "Get User By ID REST API",
+            description = "Get User By ID REST API is used to get a single user from the database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status 200 SUCCESS"
+    )
     @GetMapping
     public String getAllUsers(){
 
